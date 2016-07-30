8b1a644915ba7fdcc39777fc7c954f7c28bfcf0b
James <jagleeso@gmail.com>
2016-07-30 17:54:47 -0400

    Add some configs (xorg / setup instructions from README)

---
+++ config/guest/source_me.sh
@@ -0,0 +1,3 @@
+export GLSTUB=10.70.2.197:7000
+export IS_GLSTUB_DAEMON=yes
+export CR_SYSTEM_GL_PATH=/usr/lib/nvidia-361
---
+++ config/host/source_me.sh
@@ -0,0 +1,2 @@
+export GLSTUB=10.70.2.90:7001 
+export CR_SYSTEM_GL_PATH=/usr/lib/nvidia-361
